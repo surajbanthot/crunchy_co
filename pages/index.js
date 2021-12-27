d37973721246dcaf173571cb89b059f151fb62d2
surajbanthot <surajbanthot@gmail.com>
2021-12-27 15:29:31 +0530

    how it works home page content

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,23 @@ export default function Home() {
         <img src="/granola.png" className="h-60 w-60" />
         <div>See our Plans</div>
         <div>
-          <span>Shopping with us for the firt time? Use<Coupon />To Avail 20% discount on your purchase.</span>
+          <span>
+            Shopping with us for the firt time? Use
+            <Coupon />
+            To Avail 20% discount on your purchase.
+          </span>
+        </div>
+        <img src="/arrow-pointer.png" className="h-20 w-20" />
+        <div>How it Works</div>
+        <div>
+          <img src="/"/>
+          <div>Shop</div>
+        </div>
+        <div>
+          <div>Ship</div>
+        </div>
+        <div>
+          <div>Snack</div>
         </div>
       </main>
     </>
