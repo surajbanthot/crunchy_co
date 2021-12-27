f2443b094b1994804947e54be8379fec7032f6e6
surajbanthot <surajbanthot@gmail.com>
2021-12-27 14:59:33 +0530

    home page build

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import Coupon from "./coupons";
 export default function Home() {
   return (
     <>
-    
-    <Coupon/>
-      
+      <main className="">
+        <img src="/granola.png" className="h-60 w-60" />
+        <div>See our Plans</div>
+        <div>
+          <span>Shopping with us for the firt time? Use<Coupon />To Avail 20% discount on your purchase.</span>
+        </div>
+      </main>
     </>
   );
 }
